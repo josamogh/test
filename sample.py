1107a4e51c484c4b4a76dc961e8e478075867db9
Amogh Joshi <amoghjos273@gmail.com>
2026-08-21 16:35:38 +0530

    sample

--- sample.py
+++ sample.py
@@ -2,5 +2,6 @@ Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print("Main")
 a= 2
+s
 
 

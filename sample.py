bcb5ce7cd7188b8a1fe84f52c0e139c2abdb2535
josamogh <amoghjos273@gmail.com>
2026-08-21 13:15:04 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> print("Hello World")
+>>> print("This is changed")
 a= 2
 b=3
 
